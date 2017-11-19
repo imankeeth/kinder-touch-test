c4bc4384f2e31330d3abeca1a361015e6d36fbfd
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-11-19 20:32:57 +0530

    Login Form done

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import '../styles/login.css';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailid: '',
+      password: '',
+    };
+    this.onFieldChange = this.onFieldChange.bind(this);
+  }
+
+  onFieldChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div className="container">
@@ -11,26 +24,32 @@ export default class Login extends Component {
               <div className="field">
                 <input
                   type="text"
-                  id="fieldUser"
+                  id="emailid"
+                  name="emailid"
                   className="input"
+                  value={this.state.emailid}
+                  onChange={this.onFieldChange}
                   required
-                  pattern=".*\S.*"
                 />
-                <label htmlFor="fieldUser" className="label">
-                  Username
+                <label htmlFor="emailid" className="label">
+                  Email ID
                 </label>
+                {/* <span>Please fill this field</span> */}
               </div>
               <div className="field">
                 <input
                   type="password"
                   id="fieldPassword"
+                  name="password"
                   className="input"
+                  value={this.state.password}
+                  onChange={this.onFieldChange}
                   required
-                  pattern=".*\S.*"
                 />
                 <label htmlFor="fieldPassword" className="label">
                   Password
                 </label>
+                {/* <span>Please fill this field</span> */}
               </div>
               <div className="profile__footer">
                 <div className="button raised blue">
